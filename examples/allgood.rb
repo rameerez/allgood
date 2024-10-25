61fb20d96827bc7e741b96539721166e4dfa6f61
Javi R <4920956+rameerez@users.noreply.github.com>
2024-10-25 21:04:11 +0100

    Add `vips` checks to example `allgood.rb` config

--- examples/allgood.rb
+++ examples/allgood.rb
@@ -1,3 +1,11 @@
+require 'open-uri'
+require 'vips'
+require 'image_processing/vips'
+
+TEST_IMAGE = URI.open("https://picsum.photos/id/237/536/354").read
+
+# --- ACTIVE RECORD ---
+
 check "We have an active database connection" do
   make_sure ActiveRecord::Base.connection.connect!.active?
 end
@@ -36,6 +44,61 @@ check "Database migrations are up to date" do
   make_sure ActiveRecord::Migration.check_all_pending! == nil
 end
 
+# --- IMAGE PROCESSING ---
+
+check "Vips (libvips) is installed on Linux" do
+  if Rails.env.production?
+    output = `ldconfig -p | grep libvips`
+    make_sure output.present? && output.include?("libvips.so") && output.include?("libvips-cpp.so"), "Vips is installed on Linux"
+  else
+    make_sure true, "Not a Linux production environment, skipping (#{Rails.env.to_s})"
+  end
+end
+
+check "Vips is available to Rails" do
+  make_sure Vips::VERSION.present?, "Vips available with version #{Vips::VERSION}"
+end
+
+check "Vips can perform operations on images" do
+  url = "https://picsum.photos/id/237/536/354"
+  image = Vips::Image.new_from_buffer(TEST_IMAGE, "")
+  processed_image = image
+    .gaussblur(10)                   # Apply Gaussian blur with sigma 10
+    .linear([1.2], [0])              # Increase brightness
+    .invert                          # Invert colors for a wild effect
+    .sharpen                         # Apply sharpening
+    .resize(0.5)
+
+  make_sure processed_image.present? && processed_image.width == 268 && processed_image.height == 177, "If we input an image of 536x354, and we apply filters and a 0.5 resize, we should get an image of 268x177"
+end
+
+check "ImageProcessing::Vips is available to Rails" do
+  make_sure ImageProcessing::Vips.present?, "ImageProcessing::Vips available"
+end
+
+check "ImageProcessing can perform operations on images" do
+  url = "https://picsum.photos/id/237/536/354"
+  image_processing_image = ImageProcessing::Vips
+    .source(Vips::Image.new_from_buffer(TEST_IMAGE, ""))
+    .resize_to_limit(123, 123)        # Resize to fit within 500x500
+    .convert("webp")                  # Convert to webp format
+    .saver(strip: true)               # Strip metadata
+    .call
+  processed_image = Vips::Image.new_from_file(image_processing_image.path)
+
+  make_sure processed_image.present? && processed_image.width == 123 && processed_image.get("vips-loader") == "webpload" && !processed_image.get_fields.include?("exif-data"), "ImageProcessing can resize, remove metadata, and convert to webp"
+end
+
+# --- CACHE ---
+
+check "Cache is accessible and functioning" do
+  cache_value = "allgood_#{Time.now.to_i}"
+  Rails.cache.write("allgood_health_check_test", cache_value, expires_in: 1.minute)
+  make_sure Rails.cache.read("allgood_health_check_test") == cache_value, "The `allgood_health_check_test` key in the cache should return the string `#{cache_value}`"
+end
+
+# --- SYSTEM ---
+
 check "Disk space usage is below 90%" do
   usage = `df -h / | tail -1 | awk '{print $5}' | sed 's/%//'`.to_i
   expect(usage).to_be_less_than(90)
@@ -44,10 +107,4 @@ end
 check "Memory usage is below 90%" do
   usage = `free | grep Mem | awk '{print $3/$2 * 100.0}' | cut -d. -f1`.to_i
   expect(usage).to_be_less_than(90)
-end
-
-check "Cache is accessible and functioning" do
-  cache_value = "allgood_#{Time.now.to_i}"
-  Rails.cache.write("allgood_health_check_test", cache_value, expires_in: 1.minute)
-  make_sure Rails.cache.read("allgood_health_check_test") == cache_value, "The `allgood_health_check_test` key in the cache should return the string `#{cache_value}`"
 end
