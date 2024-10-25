b9185a27529a4d6b3ba62e6bc757c6fe7b3d8f47
Javi R <4920956+rameerez@users.noreply.github.com>
2024-10-26 00:51:37 +0100

    Prevent segfault when requiring gems in allgood.rb The gem was loading configuration before Rails finished initializing all gems, which could cause segfaults with certain gems like Vips. Changed to use config.after_initialize to ensure all gems are properly loaded first.

--- lib/allgood/engine.rb
+++ lib/allgood/engine.rb
@@ -2,7 +2,7 @@ module Allgood
   class Engine < ::Rails::Engine
     isolate_namespace Allgood
 
-    initializer "allgood.load_configuration" do
+    config.after_initialize do
       config_file = Rails.root.join("config", "allgood.rb")
       if File.exist?(config_file)
         Allgood.configure do |config|
